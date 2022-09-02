306a3f2f3c97906254da1d11c2a4602d1237fad8
MarreTeint <martinzs1@yahoo.fr>
2022-09-02 16:28:48 +0200

    start

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,9 @@ import java.util.ArrayList;
 
 public class Main {
 
-    public static void main(String[] args){
-
-        //Le projet bien franglais, bien trop relou trop relou
-
+    Token current = new Token(null,null,null);
+    Token last = new Token(null,null,null);
+    static void next(String File){
         String fileName = args[0];
         ArrayList<Token> listTokens = new ArrayList<Token>();
 
@@ -54,14 +53,26 @@ public class Main {
                         case ';': //Closed accolade
                             listTokens.add(new Token("endOfLine", 0, lineIndex));
                             break;
+                        case '+': //Plus
+                            listTokens.add(new Token("plus", 0, lineIndex));
+                            break;
+                        case '-': //Minus
+                            listTokens.add(new Token("minus", 0, lineIndex));
+                            break;
+                        case '*': //Multiply
+                            listTokens.add(new Token("multiply", 0, lineIndex));
+                            break;
+                        case '/': //Divide
+                            listTokens.add(new Token("divide", 0, lineIndex));
+                            break;
                         case 'a': case 'b': case 'c': case 'd': case 'e': case 'f': case 'g': //Lower case leters
-                            case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n':
-                                case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u':
-                                    case 'v': case 'w': case 'x': case 'y': case 'z':
+                        case 'h': case 'i': case 'j': case 'k': case 'l': case 'm': case 'n':
+                        case 'o': case 'p': case 'q': case 'r': case 's': case 't': case 'u':
+                        case 'v': case 'w': case 'x': case 'y': case 'z':
                         case 'A': case 'B': case 'C': case 'D': case 'E': case 'F': case 'G': //Upper case letters
-                            case 'H': case 'I': case 'J': case 'K': case 'L': case 'M': case 'N':
-                                case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case 'U':
-                                    case 'V': case 'W': case 'X': case 'Y': case 'Z':
+                        case 'H': case 'I': case 'J': case 'K': case 'L': case 'M': case 'N':
+                        case 'O': case 'P': case 'Q': case 'R': case 'S': case 'T': case 'U':
+                        case 'V': case 'W': case 'X': case 'Y': case 'Z':
                             word = word + lettre; //Concaténation du potentiel mot
                             isAWord = true; //On cherche à compléter un mot
                             break;
@@ -77,11 +88,37 @@ public class Main {
 
         } catch (IOException e) {
             e.printStackTrace();
-        }   
+        }
+    }
+
+    public boolean check(type){
+        if(current.type == type){
+            last = current;
+            current = next();
+            return true;
+        }
+    }
+
+    public boolean accept(type){
+        if(check(type)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public static void main(String[] args){
+
+        //Le projet bien franglais, bien trop relou trop relou
+
+
+
+
 
         //Print results
         for(Token token : listTokens)
             System.out.println(token.toString());
+
 
     }
 
