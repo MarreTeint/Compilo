a33bdf76f21ca6e0ed323a8d833eaf07ea6c241c
2maxime0 <maxouf304@gmail.com>
2022-10-05 14:00:58 +0200

    Test de nouveau projet, est ce que ça change quelque chose ?

--- src/ErrLexical.java
+++ src/ErrLexical.java
@@ -1,12 +1,12 @@
- public class ErrLexical extends Exception {
-     public ErrLexical(String message, int line) {
-         super(message + " at line " + line);
-     }
-     public ErrLexical(String message) {
-         super(message);
-     }
-     public ErrLexical(){
-         super();
-     }
- }
+public class ErrLexical extends Exception {
+    public ErrLexical(String message, int line) {
+        super(message + " at line " + line);
+    }
+    public ErrLexical(String message) {
+        super(message);
+    }
+    public ErrLexical(){
+        super();
+    }
+}
 
