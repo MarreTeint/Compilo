e5276c4eef9cec966a76696a528b4a0b77121e59
2maxime0 <maxouf304@gmail.com>
2022-10-06 11:11:57 +0200

    Minor refactoring because i was bored

--- src/Token.java
+++ src/Token.java
@@ -1,49 +1,38 @@
 public class Token {
 
     String type;
-    int valeur;
-    int ligne;
-    int adresse;
+    int    valeur;
+    int    ligne;
+    int    adresse;
 
     public Token(String type, int valeur, int ligne) {
-        this.type = type;
+        this.type =   type;
         this.valeur = valeur;
-        this.ligne = ligne;
+        this.ligne =  ligne;
     }
 
     public Token(String type, int valeur, int ligne, int adresse) {
-        this.type = type;
-        this.valeur = valeur;
-        this.ligne = ligne;
+        this.type =    type;
+        this.valeur =  valeur;
+        this.ligne =   ligne;
         this.adresse = adresse;
     }
 
     public Token(String type, int valeur) {
-        this.type = type;
+        this.type =   type;
         this.valeur = valeur;
     }
 
-    public String getType() {
-        return type;
-    }
-
-    public int getValeur() {
-        return valeur;
-    }
-
-    public int getLigne() {
-        return ligne;
-    }
-
-    public int getAdresse() {
-        return adresse;
-    }
+    public String getType() { return type; }
+    public int getValeur() { return valeur; }
+    public int getLigne() { return ligne; }
+    public int getAdresse() { return adresse;}
 
     @Override
     public String toString() {
-        return "Type = " + getType() + "\n" +
+        return  "Type = "   + getType()                     + "\n" +
                 "Valeur = " + Integer.toString(getValeur()) + "\n" +
-                "Ligne = " + Integer.toString(getLigne());
+                "Ligne = "  + Integer.toString(getLigne());
     }
 
     public static final String TYPE_EOS =         "eos";
