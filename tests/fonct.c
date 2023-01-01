d5238e99a59865c948f84f844ab3994bfa92e0d3
Martin SEBIROT <96467585+MarreTeint@users.noreply.github.com>
2023-01-01 18:00:36 +0100

    added test

---
+++ tests/fonct.c
@@ -0,0 +1,12 @@
+int fonct(){
+    return 1;
+}
+
+int again(int i){
+    fonct();
+    return i;
+}
+
+int andAgain(int i, int j){
+    return i+j;
+}
---
+++ tests/vars.c
@@ -0,0 +1,9 @@
+int main(){
+    int i;
+    int j, k;
+    int i = 0;
+    int j = 0, k;
+    int i = 0, j = 0, k;
+    int i = 0, j = 0, k = 0;
+    int i, j=0;
+}
